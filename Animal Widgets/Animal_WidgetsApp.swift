6fa1f33f68dad36488e25cac09c58707e067ccbe
Chris Hyorok Lee <hl3838@columbia.edu>
2025-12-30 15:33:16 -0500

    Initial Commit

---
+++ Animal Widgets/Animal_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_WidgetsApp.swift
+//  Animal Widgets
+//
+//  Created by 이효록 on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Animal_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
